a37e321322629f6ea80dfbff17ddae93b4d38c9f
carlosmunozmagro <carlosmunozmagro@gmail.com>
2026-07-15 08:57:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE = 'colombia-v5';
+const CACHE = 'colombia-v6';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const REMOTE_OK = h => h.endsWith('wikipedia.org') || h.endsWith('wikimedia.org');
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
 });
@@ -7,13 +8,13 @@ self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', e => {
+  const u = new URL(e.request.url);
+  const cacheable = e.request.method === 'GET' && (u.origin === location.origin || REMOTE_OK(u.hostname));
+  if (!cacheable) return;
   e.respondWith(
     caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
-      if (e.request.method === 'GET' && res.ok && new URL(e.request.url).origin === location.origin) {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-      }
+      if (res.ok) { const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); }
       return res;
-    }).catch(() => caches.match('./index.html')))
+    }).catch(() => u.origin === location.origin ? caches.match('./index.html') : Response.error()))
   );
 });
